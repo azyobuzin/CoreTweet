1e992ffeddab18a55df2676a5f130856161e626e
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-09-15 23:46:59 +0900

    [LibAzyotter] Set version from AppVeyor

---
+++ CITool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CITool
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var version = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION");
+            if (string.IsNullOrEmpty(version)) return;
+
+            var projectFile = args[0];
+
+            var j = JObject.Parse(File.ReadAllText(projectFile));
+            j["version"] = version;
+
+            File.WriteAllText(projectFile, j.ToString());
+        }
+    }
+}
